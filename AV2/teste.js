dcd404206d2539c8b77fe4d54355e1c0d029e96b
heitorctm <heitort@gmail.com>
2022-09-23 09:25:23 -0300

    temas para estudar

---
+++ AV2/teste.js
@@ -0,0 +1,14 @@
+const cond = true
+const result = cond ? 'true' : 'false'
+// operador ternario
+console.log ('sou true?', cond ? 'true' : 'false')  
+
+// google 'if else inline js'
+
+0 = false
+
+(function)()
+
+map
+filter
+reduce
