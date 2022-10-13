84143ae8dea35eebeb601ca9c99eea08f4b72bfb
heitorctm <heitort@gmail.com>
2022-10-13 17:08:31 -0300

    testes

---
+++ AV2/testes.js
@@ -0,0 +1,16 @@
+﻿var vetor = [1,2,3,4,5,6,7,10,5,13];
+
+console.log(getMaxOfArray(vetor))
+console.log(getMinOfArray(vetor))
+var novoVetor = vetor.filter(removeMax);
+console.log(novoVetor)
+
+function getMaxOfArray(numArray) {
+    return Math.max.apply(null, numArray);
+}
+function getMinOfArray(numArray) {
+    return Math.min.apply(null, numArray);
+}
+function removeMax(valor) {
+    return getMaxOfArray(valor);
+}
